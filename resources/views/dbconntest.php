a0544535997c285d7bc95900d0d9b438bffe7aff
Dhruva <dhruva.5059@gmail.com>
2016-02-22 22:55:29 -0500

    Added my project

---
+++ resources/views/dbconntest.php
@@ -0,0 +1,28 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "dhruva";
+$database = "dhruva";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+
+$sql = "INSERT INTO temp (Name)
+VALUES ('John')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+
+?> 
